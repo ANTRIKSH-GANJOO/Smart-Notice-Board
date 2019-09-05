cda9f17f2cef00b2c6bd863b467b83ccc4662875
ANTRIKSH-GANJOO <47152258+ANTRIKSH-GANJOO@users.noreply.github.com>
2019-09-06 02:28:19 +0530

    Create webbrowser.py

---
+++ webbrowser.py
@@ -0,0 +1,5 @@
+import os, sys
+
+url = "https://firebasestorage.googleapis.com/v0/b/smart-notice-board-68568.appspot.com/o/fdp._kcatS_dna_yarrA_1?alt=media&token=7cc1338c-24da-4222-98a9-4b5daae44712"
+os.system('wget '+str(url))
+#os.system('evince pdf.pdf')
